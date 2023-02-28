69a651b55a2e5fca4715b3d0ccb9904c353d9819
João Bruno <jbrunomf@outlook.com>
2023-02-27 20:38:11 -0400

    Add project files.

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.NetworkInformation;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> _filmes = new();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody]Filme filme)
+        {
+            _filmes.Add(filme);
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> GetFilmes()
+        {
+            return _filmes;
+        }
+    }
+}
---
+++ Models/Filme.cs
@@ -0,0 +1,9 @@
+﻿namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        public Guid Id { get; set; }
+        public string Titulo { get; set; }
+        public string Genero { get; set; }
+    }
+}
