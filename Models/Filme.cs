a44837ffc9ec4844994de4d6bff6a36544ff26bd
João Bruno <jbrunomf@outlook.com>
2023-02-28 21:46:13 -0400

    Database criado

--- Models/Filme.cs
+++ Models/Filme.cs
@@ -1,9 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FilmesAPI.Models
 {
     public class Filme
     {
+        [Key]
         public Guid Id { get; set; }
 
         [Required(ErrorMessage = "O título é obrigatório.")]
