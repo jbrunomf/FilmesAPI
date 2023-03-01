3251587bacfb35f05d3d04d1d5f6671f2553873e
João Bruno <jbrunomf@outlook.com>
2023-02-28 22:09:47 -0400

    add: FilmeDTO

---
+++ DTO/FilmeDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.DTO
+{
+    public class FilmeDTO
+    {
+        [Required(ErrorMessage = "O título é obrigatório.")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "{0} é obrigatório.")]
+        [MaxLength(50, ErrorMessage = "O {0} deve até {1} caracteres.")]
+        public string Genero { get; set; }
+
+        [Required]
+        [Range(10, 300, ErrorMessage = "A {0} deve ter entre {1} e {2} minutos.")]
+        public int Duracao { get; set; }
+    }
+}
